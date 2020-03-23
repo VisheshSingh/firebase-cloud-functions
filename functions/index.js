15b21359a4de96d6a39699d10c0886384e3f79a1
Vishesh Singh <VisheshSingh16@gmail.com>
2020-03-23 15:45:02 -0500

    Callable Functions 😎

--- functions/index.js
+++ functions/index.js
@@ -1,5 +1,12 @@
 const functions = require('firebase-functions');
 
+// HTTP REQUESTS
 exports.toTheDojo = functions.https.onRequest((req, res) => {
   res.redirect('https://www.thenetninja.co.uk');
+});
+
+// CALLABLE
+exports.sayHello = functions.https.onCall((data, context) => {
+  const name = data.name;
+  return `hello ${name}!`;
 });
--- public/js/main.js
+++ public/js/main.js
@@ -2,12 +2,21 @@ const addRequest = document.querySelector('.add-request');
 const newRequest = document.querySelector('.new-request');
 const body = document.querySelector('body');
 
+// OPEN MODAL
 addRequest.addEventListener('click', () => {
   newRequest.classList.add('open');
 });
 
+// CLOSE MODAL
 newRequest.addEventListener('click', e => {
   if (e.target.classList.contains('new-request')) {
     newRequest.classList.remove('open');
   }
+});
+
+// CALL sayHello callable function
+const button = document.querySelector('.call');
+button.addEventListener('click', () => {
+  const sayHello = firebase.functions().httpsCallable('sayHello');
+  sayHello({ name: 'Shaun' }).then(result => console.log(result.data));
 });
